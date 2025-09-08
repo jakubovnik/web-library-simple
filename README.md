cbfb764c466965e8db5fcbddf5fd646d93076ab9
Jakub Lukac <j.lukac152@gmail.com>
2025-09-08 22:48:20 +0200

    Most css for the main page finished

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # web-library-simple
+Tento projekt slouží pro splnění jednodušší varianty testovacího úkolu
--- src/index.php
+++ src/index.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <link rel="stylesheet" href="css/default.css">
-    <title>Title</title>
+    <title>Knihovna</title>
 </head>
 <body>
+    <div id="navbar">
+        <div id="navbar-title">Moje knihovna</div>
+        <div id="navbar-add-btn">
+            <a href="add-book.php">
+                <img src="images/plus-circle.svg" alt="add book">
+            </a>
+        </div>
+    </div>
     <div id="book-container">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
-        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <div class="book-preview">
+            <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+            <div class="book-preview-overlay">
+                <div class="book-preview-title">example</div>
+            </div>
+        </div>
     </div>
 <script>
 
