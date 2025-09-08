d0a0f7ff96c5925cd03a66b9b0b2231c31d93ff0
Jakub Lukac <j.lukac152@gmail.com>
2025-09-08 19:09:40 +0200

    basic grid layout implemented

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/default.css">
+    <title>Title</title>
+</head>
+<body>
+    <div id="book-container">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+        <img src="images/preview-example.png" alt="bruh" class="book-preview-image">
+    </div>
+<script>
+
+</script>
+</body>
+</html>
